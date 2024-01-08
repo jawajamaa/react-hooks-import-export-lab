3cda37e06ed54d942de17c4cb0252044f7c38808
Timothy Ryon <tim@timryon.com>
2024-01-08 14:15:51 -0500

    finished lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,9 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="./Home.js">I'm a link!</a>
     </nav>
   );
 }
+
+export default NavBar
